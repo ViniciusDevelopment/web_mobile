d4633f81763ad1c3c9b4193f542c15223f27254b
NOTEBOOK-VINICI\VINICIUS DEVELOPMENT <viniciusmacielpires@gmail.com>
2024-03-13 21:09:16 -0300

    aula 2 commit 1

--- js/funcoes.js
+++ js/funcoes.js
@@ -2,3 +2,21 @@
     function updateValue(value) {
         document.getElementById('displayValue').textContent = value
     }
+
+    function funcao1()
+    {
+        alert("Minha função");
+        $("#mensagem").html("Area de mensagem");
+        $(".btn-danger").html("Executar");
+    }
+
+    function funcao2()
+    {
+        $(".btn-danger").html("Executado");
+        $("#mensagem").empty();
+        var alunos = ['Aluno 01', 'Aluno 02', 'Aluno 03', 'Aluno 04', 'Aluno 05'];
+        $.each(alunos, function(index, value){
+            $("#mensagem").append(value + "<br>");
+        });
+        // $("#mensagem").html("Mensagem alterada");
+    }
